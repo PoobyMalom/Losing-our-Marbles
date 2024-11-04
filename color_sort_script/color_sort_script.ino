84d96fd08eeeac0cf1f2e8760583b43cc1f88c04
chillybeans-23 <78663146+chillybeans-23@users.noreply.github.com>
2024-11-04 17:19:02 -0500

    outline color_sort_script.ino

--- color_sort_script/color_sort_script.ino
+++ color_sort_script/color_sort_script.ino
@@ -1,41 +1,40 @@
 // color sorting script
+// future improvements: add safety to ensure solenoid isn't on for more than 1s
+#include <Servo.h>
+
+
 // initialize variables
-DROPPERSOLENOID = 12;
-BLACKCOLUMN; // fill in with servo angle for black column
-WHITECOLUMN; // fill in with servo angle for white column
-BIN; // fill in with servo angle for marble disposal
+int DROPPER_SOLENOID_PIN = 12;
+int DROPPER_SERVO_PIN = 5;
+int BLACK_COL_ANG = 45; // servo angle for black column (no gear ratios)
+int WHITE_COL_ANG = 80; // servo angle for white column (no gear ratios)
+int OUT_COL_ANG = 0; // servo angle for marble disposal
+int SOLENOID_ON_TIME = 100;
+
+Servo myservo;  // create Servo object to control a servo
+
+int pos = 0;    // variable to store the servo position
 
 void setup(){   
-  pinMode(DROPPERSOLENOID,OUTPUT); // RELAY PIN   
-  digitalWrite(DROPPERSOLENOID,HIGH); // Normally ON Only For Chanies Relay Module   
+  pinMode(DROPPER_SOLENOID_PIN,OUTPUT); // RELAY PIN   
+  digitalWrite(DROPPER_SOLENOID_PIN,HIGH); // Normally ON Only For Chanies Relay Module   
+  myservo.attach(DROPPER_SERVO_PIN);  // attaches the servo on pin 5 to the Servo object
 }  
 
 void loop(){
-  
+  digitalWrite(DROPPER_SOLENOID_PIN,LOW); // RELAY ON. DONT POWER FOR MORE THAN 1 SEC
   // color sensors determines color of marble. create "marbleColour"
-  // determine where marble should go
-  if (marbleColor == black){
-    //servo goes to BLACKCOLUMN
-  }
-  else if (marbleColor == white){
-    // servo goes to WHITECOLUMN
-  }
-  else{
-    // servo tosses marble out
-  }
-
-  // solenoid pushes marble out
-  pulseSolenoid(DROPPERSOLENOID);
-
-
-}
-
-// solenoid pushes marble out
-void pulseSolenoid(int RELAYPIN){
-  digitalWrite(RELAYPIN,LOW); // RELAY ON   
-  delay(200);   
-  digitalWrite(RELAYPIN,HIGH); // RELAY OFF   
-  delay(50);
+  //if (marbleColor == black){
+    myservo.write(BLACK_COL_ANG);
+  //}
+  //else if (marbleColor == white){
+    myservo.write(WHITE_COL_ANG);
+  //}
+  //else {
+    myservo.write(OUT_COL_ANG);
+  //}
+  digitalWrite(DROPPER_SOLENOID_PIN,HIGH); // RELAY OFF
+  
 }
 
 

1abcd137034799b18293a020f18ed0d663ae0505
chillybeans-23 <78663146+chillybeans-23@users.noreply.github.com>
2024-11-06 20:39:57 -0500

    semi-integrate color sensing

---
+++ color_sensor_test/color_sensor_test.ino
@@ -0,0 +1,91 @@
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  if (tcs.begin()){
+    Serial.println("found sensor");
+  } else {
+    Serial.println("no sensor found");
+    while (1); // end program
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  uint16_t r, g, b, c;
+  tcs.getRawData(&r, &g, &b, &c);
+
+  //check for significant color
+  int threshold = 50;
+  if (c > threshold){
+    //normalize values
+    float sum = c;
+    float red = r/ sum;
+    float green = g/ sum;
+    float blue = b/ sum;
+
+    Serial.print("R: "); Serial.print(r); Serial.print(" ");
+    Serial.print("G: "); Serial.print(g); Serial.print(" ");
+    Serial.print("B: "); Serial.print(b); Serial.print(" ");
+    Serial.print("C: "); Serial.print(c); Serial.print(" ");
+
+    //calc colorTemp and lux
+    uint16_t colorTemp = tcs.calculateColorTemperature(r, g, b);
+    uint16_t lux = tcs.calculateLux(r, g, b);
+
+    Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K -");
+    Serial.print("Lux: "); Serial.println(lux, DEC);
+
+    // direct against led
+/*
+    if (r+g+b < 100){ //black
+      Serial.println("black");
+    } else if (r+g+b > 10000){ //white
+      Serial.println("white");
+    } else if (r > 1000 && g < 700 && b < 700){ //red
+      Serial.println("red");
+    } else if (g*1.5 > r+b && r+g+b < 5000){ //green
+      Serial.println("green");
+    } else if (r+g+b > 500 && b > r && b > g){ //blue
+      Serial.println("blue");
+    } else if (5000 < r+g+b < 10000 && b*4 < r+g){ //yellow
+      Serial.println("yellow");
+    } else {
+      Serial.println("idk color");
+    }
+    */
+
+    if (c > 850){
+      if (g+b > 850){ //white
+        Serial.println("white");
+      } else { // yellow
+        Serial.println("yellow");
+      }
+    } else {
+      if (c < 400 && c> 300){ // black
+        Serial.println("black");
+      } else if (g>b*1.5 && g > 200){
+        Serial.println("green");
+      } else if (r>g*1.5 && r > 200){
+        Serial.println("red");
+      } else if (r<b && b<g){
+        Serial.println("blue");
+      } else{
+        Serial.println("dunno");
+      }
+    }
+  }
+
+  else {
+    Serial.println("no significant color");
+  }
+  
+
+
+
+}
--- color_sort_script/color_sort_script.ino
+++ color_sort_script/color_sort_script.ino
@@ -1,6 +1,11 @@
 // color sorting script
 // future improvements: add safety to ensure solenoid isn't on for more than 1s
 #include <Servo.h>
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
+
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
+
 
 
 // initialize variables
@@ -11,6 +16,8 @@ int WHITE_COL_ANG = 80; // servo angle for white column (no gear ratios)
 int OUT_COL_ANG = 0; // servo angle for marble disposal
 int SOLENOID_ON_TIME = 100;
 
+String marbleColor;
+
 Servo myservo;  // create Servo object to control a servo
 
 int pos = 0;    // variable to store the servo position
@@ -19,20 +26,82 @@ void setup(){
   pinMode(DROPPER_SOLENOID_PIN,OUTPUT); // RELAY PIN   
   digitalWrite(DROPPER_SOLENOID_PIN,HIGH); // Normally ON Only For Chanies Relay Module   
   myservo.attach(DROPPER_SERVO_PIN);  // attaches the servo on pin 5 to the Servo object
+
+  Serial.begin(9600);
+
+  if (tcs.begin()){
+    Serial.println("found sensor");
+  } else {
+    Serial.println("no sensor found");
+    while (1); // end program
+  }
 }  
 
 void loop(){
+
+  uint16_t r, g, b, c;
+  tcs.getRawData(&r, &g, &b, &c);
+
+  //check for significant color
+  int threshold = 50;
+  if (c > threshold){
+    //normalize values
+    float sum = c;
+    float red = r/ sum;
+    float green = g/ sum;
+    float blue = b/ sum;
+
+    Serial.print("R: "); Serial.print(r); Serial.print(" ");
+    Serial.print("G: "); Serial.print(g); Serial.print(" ");
+    Serial.print("B: "); Serial.print(b); Serial.print(" ");
+    Serial.print("C: "); Serial.print(c); Serial.print(" ");
+
+
+    if (c > 850){
+      if (g+b > 850){ //white
+        Serial.println("white");
+        marbleColor = "white";
+      } else { // yellow
+        Serial.println("yellow");
+        marbleColor = "yellow";
+      }
+    } else {
+      if (c < 400 && c> 300){ // black
+        Serial.println("black");
+        marbleColor = "black";
+      } else if (g>b*1.5 && g > 200){
+        Serial.println("green");
+        marbleColor = "green";
+      } else if (r>g*1.5 && r > 200){
+        Serial.println("red");
+        marbleColor = "red";
+      } else if (r<b && b<g){
+        Serial.println("blue");
+        marbleColor = "blue";
+      } else{
+        Serial.println("dunno");
+        marbleColor = "dunno";
+      }
+    }
+  }
+
+  else {
+    Serial.println("no significant color");
+  }
   digitalWrite(DROPPER_SOLENOID_PIN,LOW); // RELAY ON. DONT POWER FOR MORE THAN 1 SEC
   // color sensors determines color of marble. create "marbleColour"
-  //if (marbleColor == black){
+  if (marbleColor == "black"){
     myservo.write(BLACK_COL_ANG);
-  //}
-  //else if (marbleColor == white){
+    delay(1000);
+  }
+  else if (marbleColor == "white"){
     myservo.write(WHITE_COL_ANG);
-  //}
-  //else {
+    delay(1000);
+  }
+  else {
     myservo.write(OUT_COL_ANG);
-  //}
+    delay(1000);
+  }
   digitalWrite(DROPPER_SOLENOID_PIN,HIGH); // RELAY OFF
   
 }
